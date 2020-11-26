337a8d2c01ebeef2161df7bf193b39682162a62d
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-11-26 13:18:25 +0000

    Show the first slide on the container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import { useState } from 'react';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 import './App.css';
 import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
-  return <div>Hello from App</div>;
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>
+        </h2>
+        reviews
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+          //more stuff here
+          let position = 'nextSlide';
+          if (personIndex === index) {
+            position = 'activeSlide';
+          }
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+        <button className="prev">
+          <FaChevronLeft />
+        </button>
+        <button className="next">
+          <FaChevronRight />
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
